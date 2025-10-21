00797f85336cee9c31dd9bc02dba1c750a36dc41
evka7395 <evka7395@student.harmonytx.org>
2025-10-21 21:19:53 +0000

    Added in test code for Panda, will add comment explanations. - EK 10/21/25

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # TODO Make code framework for bot, I will import the libraries and base code into here by Friday (hopefully). - EK
+
+    ### INSTALLED LIBRARIES ###
+import pandas as pd
+import numpy as np
+
+    ### TEST CODE FOR PANDA ###
+
+ser = pd.Series()
+print("Panda Series: ", ser)
+
+data = np.array(['a','p','p','l','e'])
+
+ser = pd.Series(data)
+print("Panda Series:\n ", ser)
