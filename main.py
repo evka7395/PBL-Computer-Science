f3f2618b608b740047dc9e5d0e25f53b0790ed7d
evka7395 <evka7395@student.harmonytx.org>
2025-11-20 02:54:18 +0000

    Imported in sentiment analysis code to be used as an example and as a course of analysis and learning for how the project should go and look like with the proper code and libraries. This will obviously be changed as per science and testing, but all credits are given to those who made this code. - EK 11/19/25

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # TODO Make code framework for bot, I will import the libraries and base code into here by Friday (hopefully). - EK
 
+    ### TEST & EXAMPLE CODE ###
     ### INSTALLED LIBRARIES ###
-pip install kagglehub[pandas-datasets]
-import sklearn as sklearn
+"""import sklearn as sklearn
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -45,4 +45,60 @@ print("First 5 records:", df.head())
 df = pd.read_csv('training.1600000.processed.noemoticon.csv.zip', encoding='latin-1', header=None)
 df = df[[0, 5]]
 df.columns = ['polarity', 'text']
-print(df.head())
+print(df.head())"""
+
+# 1. pip install NRClex
+# 2. python -m textblob.download_corpora
+
+
+from nrclex import NRCLex
+import nltk
+import product_review_ex as pr
+import twitter_posts_ex as tp
+import customer_survey_ex as cs
+
+#used to avoid the already installed package message
+try:
+  nltk.data.find('tokenizers/punkt')
+except LookupError:
+  nltk.download('punkt')
+
+
+def dataAnalysis(data):
+  print(data)
+  text1 = NRCLex(data)
+  topE = text1.top_emotions
+
+  print("\nAnalysis Result")
+  print("-----------------------")
+  print('{0:15}: {1}'.format("EMOTION","RATING"))
+  print("-----------------------")
+  for i in range(len(topE)):
+    #print(topE[i][0],":\t\t\t",topE[i][1])
+    print('{0:15}: {1}'.format(topE[i][0].capitalize(),round(topE[i][1],2)))
+
+
+
+def main():
+   #Return highest emotions.
+
+  #userData = pr.questionnaire()
+  userData = tp.tweet1
+
+  # userData= cs.survey()
+  # userData = "Coding Python is a fun experience." # for testing only
+
+  dataAnalysis(userData)
+  userData = tp.tweet2
+  dataAnalysis(userData)
+  userData= tp.tweet3
+  dataAnalysis(userData)
+
+
+
+if __name__ == "__main__":
+  main()
+
+
+
+
