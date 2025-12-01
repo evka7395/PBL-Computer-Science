c5c36f12035c3b9093ad3e347186dee6ae08b59e
evka7395 <evka7395@student.harmonytx.org>
2025-12-01 22:25:07 +0000

    Creation of official main.py file for demonstration and experimentation. - EK 12/1/25

---
+++ official_main.py
@@ -0,0 +1,3 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv('https://raw.githubusercontent.com/laxmimerit/twitter-data/101ebe28367c65359a61b9f54ac446932cc401d9/twitter30k_cleaned.csv')
